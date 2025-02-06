fa6eba68d91637a0e548c7e6bfc9d8529c16ff88
Sheetal Kumar M <133575235+Slkmr20@users.noreply.github.com>
2025-02-06 20:53:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        M SHEETAL KUMAR
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Data-Driven Insights Enthusiast | Eager to Transform Raw Data into Actionable Business Solutions with SQL, Python, and Visualization Tools". `}
       </p>
       <div className="my-8">
         <BlogPosts />
